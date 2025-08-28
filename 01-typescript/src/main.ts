d3f05ac4d961b96d2fd14066ceab77d9fa18ddf9
Antonny GCR <ag.chumpitazr@gmail.com>
2025-08-28 20:58:16 +0200

    08-classes

--- 01-typescript/src/main.ts
+++ 01-typescript/src/main.ts
@@ -1,4 +1,12 @@
 import './style.css';
+// import './topics/01-basic-types';
+// import './topics/02-object-interface';
+// import './topics/03-functions';
+// import './topics/04-practice-types';
+// import './topics/05-basic-destructuring';
+// import './topics/06-function-destructuring';
+// import './topics/07-import-export';
+import './topics/08-classes';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hello Vite + TypeScript!</h1>
--- 01-typescript/src/topics/08-classes.ts
+++ 01-typescript/src/topics/08-classes.ts
@@ -2,12 +2,62 @@ export class Person {
     public name: string; // ! error si no se inicializa
     private address: string; // ? public: we can access from outside the class and private: only inside the class
 
-    constructor() {
-        this.name = 'Tonny';
-        this.address = 'Okinawa, JP';
+    constructor(name: string, address: string = 'No address') {
+        this.name = name;
+        this.address = address;
     }
+    // ? shorthand for defining and initializing properties
+    // ! error because the syntax is erasable
+    // constructor(public name: string, private address: string) { }
 }
 
-const ironman = new Person();
+
+// const ironman = new Person(); // ! error, we need to pass the parameters
+const captainAmerica = new Person('Steve Rogers', 'Brooklyn, NY');
 // console.log(ironman.address); // ! error, address is private
-console.log(ironman.name); // ? works, name is public
+// console.log(captainAmerica.name); // ? works, name is public
+console.log(captainAmerica);
+
+// ** ---------------------------------------------------------- ** //
+
+// export class Hero extends Person {
+//     public age: number;
+//     public alterEdo: string;
+//     public realName: string;
+
+//     constructor(
+//         alterEdo: string,
+//         age: number,
+//         realName: string,
+//     ) {
+//         super(realName);
+//         this.alterEdo = alterEdo;
+//         this.age = age;
+//         this.realName = realName;   
+//     }
+// }
+
+export class Hero {
+    public age: number;
+    public alterEgo: string;
+    public realName: string;
+
+    public person: Person;
+
+    constructor(
+        alterEgo: string,
+        age: number,
+        realName: string,
+        person: Person
+    ) {
+
+        this.person = person;
+        this.alterEgo = alterEgo;
+        this.age = age;
+        this.realName = realName;   
+    }
+}
+const tony = new Person('Tony Stark', 'Malibu, CA');
+const ironman = new Hero('Iroman',40, 'Tony', tony);
+console.log(ironman); // ? works, name is public
+// console.log(ironman.address); // ! error, address is private
