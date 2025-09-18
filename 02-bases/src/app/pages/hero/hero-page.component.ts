946c78b70140a814b99945cbdadc11322def4f37
Antonny GCR <ag.chumpitazr@gmail.com>
2025-09-18 23:09:59 +0200

    02-bases: computed signals

--- 02-bases/src/app/pages/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
   templateUrl: './hero-page.component.html',
+  imports: [UpperCasePipe] // Import the pipe to use it in the template
 })
 export class HeroPageComponent {
   name = signal('Ironman');
   age = signal(45);
 
-  getHeroDescription() {
-    return `${this.name()} - ${this.age()}`;
-  }
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`;
+    console.log('Computed:', { description });
+    return description;
+  }); // Computed signal that derives its value from other signals
+
+  // getHeroDescription() {
+  //   return `${this.name()} - ${this.age()}`;
+  // }
+
+  capitalizeName = computed(() => this.name().toUpperCase());
 
   changeHero() {
     this.name.set('Spiderman'); // Sets the signal to a new value, no dependency tracking
