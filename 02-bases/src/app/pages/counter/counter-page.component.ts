9f8d8899a5128f4a300e92796f76122f1ea2accc
Antonny GCR <ag.chumpitazr@gmail.com>
2025-09-18 19:23:11 +0200

    02-bases: add signal

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,16 +6,20 @@ import { Component } from "@angular/core";
 })
 export class CounterPageComponent {
   counter = 10;
+  counterSignal = signal(10);
 
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSignal.update(current => current + value); // Updates the signal and notifies dependents
   }
 
   decreaseBy(value: number) {
     this.counter -= value;
+    this.counterSignal.update(current => current - value); // Updates the signal and notifies dependents
   }
 
   resetValue() {
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSignal.set(0); // Resets the signal to 0, no dependency tracking
   }
 }
