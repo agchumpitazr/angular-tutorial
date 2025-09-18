41ba26b5556d494d8ec28e1485b89e8da2e47a78
Antonny GCR <ag.chumpitazr@gmail.com>
2025-09-19 00:03:26 +0200

    02-bases: add new page - DB

---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  // selector: 'app-dragonball-page', //? It is not needed because it is a page component, not a reusable component
+  templateUrl: './dragonball-page.component.html',
+})
+export class DragonballPageComponent {}
