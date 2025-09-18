61991c78867dc84fe5e7127cc062d34aa9ae2445
Antonny GCR <ag.chumpitazr@gmail.com>
2025-09-19 00:31:21 +0200

    02-bases: use @for

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,7 +1,24 @@
-import { Component } from "@angular/core";
+import { NgClass } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
+
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   // selector: 'app-dragonball-page', //? It is not needed because it is a page component, not a reusable component
   templateUrl: './dragonball-page.component.html',
+  imports: [NgClass],
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 15000 },
+    { id: 2, name: 'Vegeta', power: 12000 },
+    { id: 3, name: 'Piccolo', power: 8000 },
+  ]);
+
+}
