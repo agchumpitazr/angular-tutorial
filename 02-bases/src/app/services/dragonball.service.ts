b339e4d5fb84637a77d3668e895e1d63d71ca776
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 01:57:07 +0200

    02-bases: loadStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,15 +1,18 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+function loadFromLocalStorage(): Character[] {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : []; // If no characters, return empty array
+}
+
 @Injectable({ providedIn: 'root' }) // Singleton. It is created once when the app starts
 export class DragonballService {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 15000 },
-    { id: 2, name: 'Vegeta', power: 12000 },
-  ]);
+
+  characters = signal<Character[]>(loadFromLocalStorage()); // Load initial value from localStorage
 
   saveInLocalStorage = effect(() => {
-    console.log('Characters count: ', this.characters().length);
+    console.log('Characters count: ', this.characters());
     localStorage.setItem('characters', JSON.stringify(this.characters()));
   })
 
