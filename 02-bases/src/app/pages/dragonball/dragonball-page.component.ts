5245a5948b77ec41df9ddc7da97309464d118b8a
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-02 13:42:35 +0200

    02-bases: add characters

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -15,11 +15,39 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal('Gohan');
+  power = signal(500);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 15000 },
     { id: 2, name: 'Vegeta', power: 12000 },
     { id: 3, name: 'Piccolo', power: 8000 },
     { id: 4, name: 'Yamcha', power: 50 },
   ]);
+
+  addCharacter() {
+    const newCharacter: Character | undefined =  this.createCharacter();
+    if(!newCharacter) return;
+    this.characters.update(characters => [
+      ...characters,
+      newCharacter
+    ]);
+    this.resetForm();
+  }
+
+  createCharacter() : Character | undefined {
+    if(!this.name() || !this.power() || this.power() <= 0) return;
+    const newCharacter: Character =  {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    }
+    return newCharacter;
+  }
+
+  resetForm() {
+    this.name.set('');
+    this.power.set(0);
+  }
 
 }
