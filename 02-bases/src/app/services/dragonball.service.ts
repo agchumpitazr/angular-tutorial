5575ea9b6bab1492c295819d065237a40a8ea4b3
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 01:15:36 +0200

    02-bases: effect, localStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ providedIn: 'root' }) // Singleton. It is created once when the app starts
@@ -7,6 +7,11 @@ export class DragonballService {
     { id: 1, name: 'Goku', power: 15000 },
     { id: 2, name: 'Vegeta', power: 12000 },
   ]);
+
+  saveInLocalStorage = effect(() => {
+    console.log('Characters count: ', this.characters().length);
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  })
 
   addCharacter(character: Character) {
     this.characters.update((characters) => [...characters, character]);
