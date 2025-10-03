43d3eed3367f4595ab5c1f2fe453ce2b0eab01b2
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 02:19:20 +0200

    02-bases: HashLocationStrategy

--- 02-bases/src/app/app.config.ts
+++ 02-bases/src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy //? Use HashLocationStrategy to avoid problems with refreshing the page (it uses # in the URL)
+    }
   ]
 };
