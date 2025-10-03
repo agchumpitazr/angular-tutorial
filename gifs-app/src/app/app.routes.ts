92b97bd4aacee9d8bcfb2b8257f43e47600d2636
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 15:58:36 +0200

    gifs-app: children paths

--- gifs-app/src/app/app.routes.ts
+++ gifs-app/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'), // dynamic import
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component'), // dynamic import
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component'), // dynamic import
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
