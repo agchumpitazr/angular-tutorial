b29a2ecdce8393b0ab11d7eb303b5a7b3618cb72
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 22:39:34 +0200

    gifs-app: add list component

---
+++ gifs-app/src/app/gifs/components/list/list.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ListItemComponent } from "./list-item/list-item.component";
+
+@Component({
+  selector: 'gifs-list',
+  imports: [ListItemComponent],
+  templateUrl: './list.component.html'
+})
+export class ListComponent { }
