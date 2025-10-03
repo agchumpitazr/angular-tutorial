4c761886addb1a1e8ec4d7b9b6613e73522fcd12
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 20:50:16 +0200

    gifs-app: path alias, envs

--- gifs-app/src/app/gifs/components/menu/menu-header/menu-header.component.ts
+++ gifs-app/src/app/gifs/components/menu/menu-header/menu-header.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '@environments/environment';
+// import { environment } from '../../../../../environments/environment'; //? Before tsconfig paths
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './menu-header.component.html',
 })
-export class MenuHeaderComponent { }
+export class MenuHeaderComponent {
+  envs = environment;
+}
---
+++ gifs-app/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs ',
+  companyNamme2: 'App Company',
+  companySlogan: 'The best gifs in the world',
+  //API KEY
+  // URLS
+};
