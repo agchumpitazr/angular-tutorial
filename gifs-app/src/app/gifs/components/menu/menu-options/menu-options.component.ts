83cd046e01220f25cfa5e298aadc30f0715843dc
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 20:33:20 +0200

    gifs-app: add menu options

--- gifs-app/src/app/gifs/components/menu/menu-options/menu-options.component.ts
+++ gifs-app/src/app/gifs/components/menu/menu-options/menu-options.component.ts
@@ -1,8 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOptions {
+  icon: string;
+  label: string;
+  router: string;
+  subLabel: string;
+}
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu-options.component.html',
 })
-export class MenuOptionsComponent { }
+export class MenuOptionsComponent {
+    menuOptions: MenuOptions[] = [
+    { icon: 'fa-solid fa-chart-line', label: 'Trending', router: '/dashboard/trending', subLabel: 'The best gifs' },
+    { icon: 'fa-solid fa-magnifying-glass', label: 'Buscador', router: '/dashboard/search', subLabel: 'Buscador gifs' },
+  ]
+
+}
