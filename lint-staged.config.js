80118e7b846908045393218a83375c1c786358ae
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-03 19:41:05 +0200

    gifs-app: fix config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    '*.ts': () => 'npm run type-check',
+    'gifs-app/**/*.ts': () => 'tsc --noEmit --project gifs-app/tsconfig.json',
+    '02-bases/**/*.ts': () => 'tsc --noEmit --project 02-bases/tsconfig.json',
+    '01-typescript/**/*.ts': () => 'tsc --noEmit --project 01-typescript/tsconfig.json',
 };
