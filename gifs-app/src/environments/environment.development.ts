0ae3f7a6e994c74eb21d5f9b49586f99ced978b1
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-05 13:10:58 +0200

    gifs-app: ignore envs

--- gifs-app/src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'Gifs ',
-  companyNamme2: 'App Company',
-  companySlogan: 'The best gifs in the world',
-  //API KEY
-  // URLS
-};
