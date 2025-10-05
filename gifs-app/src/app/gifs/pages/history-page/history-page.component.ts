84f9ae23cc7e5937ccc83aefa20d48ebb34fb916
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-05 15:30:40 +0200

    gifs-app: toSignal

--- gifs-app/src/app/gifs/pages/history-page/history-page.component.ts
+++ gifs-app/src/app/gifs/pages/history-page/history-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-history-page',
   imports: [],
   templateUrl: './history-page.component.html'
 })
-export default class HistoryPageComponent { // default:
+export default class HistoryPageComponent {
+  // ActivatedRoute: servicio que proporciona información sobre la ruta activada asociada al componente cargado en una ruta
+  // .params is an observable (we can subscribe to it)
 
+  // query = inject(ActivatedRoute).params.subscribe( params => {
+  //   console.log({params});
+  // });
+
+  // toSignal: convierte un observable en una señal
+  // map: operador que transforma los datos emitidos por el observable
+  // params es un objeto que contiene los parámetros de la ruta, en este caso { query: 'valor' }
+  // params['query'] accede al valor del parámetro 'query'
+  // this.query es una señal que contiene el valor del parámetro 'query' de la ruta actual
+  // cada vez que cambia el parámetro 'query', la señal se actualiza automáticamente
+
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(
+      map(params => params['query'])
+    )
+  );
 }
