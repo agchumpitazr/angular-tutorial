b16719dc952426c4c45ed65279c3d50d1e8af102
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-06 15:35:43 +0200

    gifs-app: add viewChild<ElementRef>

--- gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,12 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { ListComponent } from "../../components/list/list.component";
 import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [ListComponent],
+  // imports: [ListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   gifService = inject(GifService);
+  scrollDivRef = viewChild<ElementRef>('gifsContainer');
+
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    console.log(scrollDiv);
+  }
 }
