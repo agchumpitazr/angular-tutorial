0ba25a82ace1e9b47b3207561e9940655ad8a0ac
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-07 10:02:47 +0200

    country-app: first commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    'country-app/**/*.ts': () => 'tsc --noEmit --project country-app/tsconfig.json',
     'gifs-app/**/*.ts': () => 'tsc --noEmit --project gifs-app/tsconfig.json',
     '02-bases/**/*.ts': () => 'tsc --noEmit --project 02-bases/tsconfig.json',
     '01-typescript/**/*.ts': () => 'tsc --noEmit --project 01-typescript/tsconfig.json',
