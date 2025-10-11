437f2298acffc9146f12dd455053738de515d70e
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-11 11:50:18 +0200

    country-app: use Resource to manage the request

--- country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { CountryTableComponent } from '../../components/country-table/country-table.component';
 import { CountrySearchInputComponent } from '../../components/country-search-input/country-search-input.component';
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,28 +12,40 @@ import { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent {
   countryServide = inject(CountryService);
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  query = signal('');
 
-  onSearch(query: string) {
-    if (!query || query.length === 0) return;
-    if (this.isLoading()) return;
-    this.isError.set(null);
-    this.isLoading.set(true);
+  countryResource = resource({
+    params: () => ({ query: this.query()}),
+    loader: async({params}) => { // Destructure params to get query
+      // if (!this.query()) return [];
+      if (!params.query) return [];
+      return await firstValueFrom(this.countryServide.searchByCapital(params.query)); // Convert Observable to Promise
+    }
+  });
 
-    this.countryServide.searchByCapital(query)
-    .subscribe({ // Using the object form of subscribe to handle next and error separately
-      next: (countries) => { // Handle successful response. Next callback is called when data is received
-        this.countries.set(countries);
-        this.isLoading.set(false);
-      },
-      error: (err) => { // Handle error response
-        this.isLoading.set(false);
-        this.isError.set(err);
-        this.countries.set([]);
-      }
 
-    });
-  }
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
+
+  // onSearch(query: string) {
+  //   if (!query || query.length === 0) return;
+  //   if (this.isLoading()) return;
+  //   this.isError.set(null);
+  //   this.isLoading.set(true);
+
+  //   this.countryServide.searchByCapital(query)
+  //   .subscribe({ // Using the object form of subscribe to handle next and error separately
+  //     next: (countries) => { // Handle successful response. Next callback is called when data is received
+  //       this.countries.set(countries);
+  //       this.isLoading.set(false);
+  //     },
+  //     error: (err) => { // Handle error response
+  //       this.isLoading.set(false);
+  //       this.isError.set(err);
+  //       this.countries.set([]);
+  //     }
+
+  //   });
+  // }
 }
