a21d1c89066ac8896551d097527d0df7658ab365
Antonny GCR <ag.chumpitazr@gmail.com>
2025-10-26 17:54:14 +0100

    country-app: cache, .tap

--- country-app/src/app/country/services/country.service.ts
+++ country-app/src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, delay, map } from 'rxjs/operators';
+import { catchError, delay, map, tap } from 'rxjs/operators';
 import { RestCountryResponse } from '../interfaces/rest-country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Observable, of, throwError } from 'rxjs';
@@ -13,12 +13,17 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase().trim();
-    return this.http.get<RestCountryResponse[]>(`${API_URL}/capital/${query}`)
-    .pipe(
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+    return this.http.get<RestCountryResponse[]>(`${API_URL}/capital/${query}`).pipe(
       map((resp) => CountryMapper.toCountries(resp)),
+      tap((countries) => this.queryCacheCapital.set(query, countries)),
       catchError((err) => {
         return throwError(() => new Error(`Capital not found "${query}"`));
       })
@@ -27,9 +32,12 @@ export class CountryService {
 
   searchByCountry(query: string): Observable<Country[]> {
     query = query.toLowerCase().trim();
-    return this.http.get<RestCountryResponse[]>(`${API_URL}/name/${query}`)
-    .pipe(
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query) ?? []);
+    }
+    return this.http.get<RestCountryResponse[]>(`${API_URL}/name/${query}`).pipe(
       map((resp) => CountryMapper.toCountries(resp)),
+      tap((countries) => this.queryCacheCountry.set(query, countries)),
       delay(2000),
       catchError((error) => {
         return throwError(() => new Error(`Country not found "${query}"`));
@@ -38,10 +46,9 @@ export class CountryService {
   }
 
   searchCountryByAlphaCode(code: string): Observable<Country | undefined> {
-    return this.http.get<RestCountryResponse[]>(`${API_URL}/alpha/${code}`)
-    .pipe(
+    return this.http.get<RestCountryResponse[]>(`${API_URL}/alpha/${code}`).pipe(
       map((resp) => CountryMapper.toCountries(resp)),
-      map(countries => countries.at(0)),
+      map((countries) => countries.at(0)),
       catchError((error) => {
         return throwError(() => new Error(`Country not found with the code "${code}"`));
       })
