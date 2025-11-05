255ec0005f9580e3c8d7ce85179d834e3ae18bdb
Antonny GCR <ag.chumpitazr@gmail.com>
2025-11-05 12:57:52 +0100

    pipes-app: add lint check

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    'pipes-app/**/*.ts': () => 'tsc --noEmit --project pipes-app/tsconfig.json',
     'country-app/**/*.ts': () => 'tsc --noEmit --project country-app/tsconfig.json',
     'gifs-app/**/*.ts': () => 'tsc --noEmit --project gifs-app/tsconfig.json',
     '02-bases/**/*.ts': () => 'tsc --noEmit --project 02-bases/tsconfig.json',
