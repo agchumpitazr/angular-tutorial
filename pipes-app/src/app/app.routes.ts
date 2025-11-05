bce49655be8bd21c9698a64579893b80342c83e5
Antonny GCR <ag.chumpitazr@gmail.com>
2025-11-05 13:21:43 +0100

    pipes-app: pages, structures

--- pipes-app/src/app/app.routes.ts
+++ pipes-app/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Basic Pipes',
+    loadComponent: () => import('./pages/basic-page/basic-page'),
+  },
+  {
+    path: 'number',
+    title: 'Number Pipes',
+    loadComponent: () => import('./pages/number-page/number-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Uncommon Pipes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Custom Pipes',
+    loadComponent: () => import('./pages/custom-page/custom-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic'
+  }
+];
